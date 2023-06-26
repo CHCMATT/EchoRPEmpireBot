af640e30ab1066ead30dcdc614dd6396142a536a
Matthew Anthony <misterbaboo@gmail.com>
2023-06-26 19:33:07 -0400

    Remove temp reset

--- startup.js
+++ startup.js
@@ -7,8 +7,6 @@ module.exports.startUp = async (client) => {
 	let channel = await client.channels.fetch(process.env.EMBED_CHANNEL_ID);
 	let oldEmbed = await dbCmds.readMsgId("embedMsg");
 
-	await dbCmds.resetSummValue('countPurchaseAgreements');
-
 	try {
 		await channel.messages.fetch(oldEmbed);
 		await editEmbed.editEmbed(client);
