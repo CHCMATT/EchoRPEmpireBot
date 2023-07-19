1e37c131f710abdbfbdbc9a990579e1d20e5c4dc
Matthew Anthony <misterbaboo@gmail.com>
2023-07-18 22:03:25 -0400

    Add error logging to discord

--- personnelCmds.js
+++ personnelCmds.js
@@ -2,13 +2,34 @@ require('discord.js');
 let dbCmds = require('./dbCmds.js');
 
 module.exports.initPersonnel = async (client, userId) => {
-	let guild = await client.guilds.fetch(process.env.DISCORD_SERVER_ID);
-	let user = await guild.members.fetch(userId);
-	let initCharName;
-	if (user.nickname) {
-		initCharName = user.nickname;
-	} else {
-		initCharName = user.user.username;
+	try {
+		let guild = await client.guilds.fetch(process.env.DISCORD_SERVER_ID);
+		let user = await guild.members.fetch(userId);
+		let initCharName;
+		if (user.nickname) {
+			initCharName = user.nickname;
+		} else {
+			initCharName = user.user.username;
+		}
+		await dbCmds.initPersStats(userId, initCharName);
+	} catch (error) {
+		if (process.env.BOT_NAME == 'test') {
+			console.error(error);
+		} else {
+			console.log(`Error occured at ${errTime} at file ${fileName}!`);
+			console.error(error);
+
+			let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
+			let fileParts = __filename.split(/[\\/]/);
+			let fileName = fileParts[fileParts.length - 1];
+
+			let errorEmbed = [new EmbedBuilder()
+				.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
+				.setDescription(`\`\`\`${error.toString().slice(0, 2000)}\`\`\``)
+				.setColor('B80600')
+				.setFooter({ text: `${errTime}` })];
+
+			await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
+		}
 	}
-	await dbCmds.initPersStats(userId, initCharName);
 };
--- postEmbed.js
+++ postEmbed.js
@@ -2,44 +2,65 @@ let dbCmds = require('./dbCmds.js');
 let { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, time } = require('discord.js');
 
 module.exports.postEmbed = async (client) => {
-	let salespersonStats = await dbCmds.currStats();
-	let overallDescList = '';
+	try {
+		let salespersonStats = await dbCmds.currStats();
+		let overallDescList = '';
 
-	let now = Math.floor(new Date().getTime() / 1000.0);
-	let today = time(now, 'd');
+		let now = Math.floor(new Date().getTime() / 1000.0);
+		let today = time(now, 'd');
 
-	for (let i = 0; i < salespersonStats.length; i++) {
-		if (salespersonStats[i].purchaseAgreements > 0) {
-			overallDescList = overallDescList.concat(`__${salespersonStats[i].charName}__: 
+		for (let i = 0; i < salespersonStats.length; i++) {
+			if (salespersonStats[i].purchaseAgreements > 0) {
+				overallDescList = overallDescList.concat(`__${salespersonStats[i].charName}__: 
 • Purchase Agreements: ${salespersonStats[i].purchaseAgreements}\n\n`);
+			}
+		}
+
+		let countPurchaseAgreements = await dbCmds.readSummValue("countPurchaseAgreements");
+
+		countPurchaseAgreements = countPurchaseAgreements.toString();
+
+		// theme color palette: https://coolors.co/palette/7400b8-6930c3-5e60ce-5390d9-4ea8de-48bfe3-56cfe1-64dfdf-72efdd-80ffdb
+
+		if (overallDescList == '') {
+			overallDescList = "There is no salesperson data to display yet."
+		}
+
+		let overallStatsEmbed = new EmbedBuilder()
+			.setTitle(`Overall Salesperson Statistics as of ${today}:`)
+			.setDescription(overallDescList)
+			.setColor('5390D9');
+
+		let purchaseAgreementsEmbed = new EmbedBuilder()
+			.setTitle('Amount of Purchase Agreements Created:')
+			.setDescription(countPurchaseAgreements)
+			.setColor('4EA8DE');
+
+		let btnRows = addBtnRows();
+
+		client.embedMsg = await client.channels.cache.get(process.env.EMBED_CHANNEL_ID).send({ embeds: [overallStatsEmbed, purchaseAgreementsEmbed], components: btnRows });
+
+		await dbCmds.setMsgId("embedMsg", client.embedMsg.id);
+	} catch (error) {
+		if (process.env.BOT_NAME == 'test') {
+			console.error(error);
+		} else {
+			console.log(`Error occured at ${errTime} at file ${fileName}!`);
+			console.error(error);
+
+			let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
+			let fileParts = __filename.split(/[\\/]/);
+			let fileName = fileParts[fileParts.length - 1];
+
+			let errorEmbed = [new EmbedBuilder()
+				.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
+				.setDescription(`\`\`\`${error.toString().slice(0, 2000)}\`\`\``)
+				.setColor('B80600')
+				.setFooter({ text: `${errTime}` })];
+
+			await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
 		}
 	}
-
-	let countPurchaseAgreements = await dbCmds.readSummValue("countPurchaseAgreements");
-
-	countPurchaseAgreements = countPurchaseAgreements.toString();
-
-	// theme color palette: https://coolors.co/palette/7400b8-6930c3-5e60ce-5390d9-4ea8de-48bfe3-56cfe1-64dfdf-72efdd-80ffdb
-
-	if (overallDescList == '') {
-		overallDescList = "There is no salesperson data to display yet."
-	}
-
-	let overallStatsEmbed = new EmbedBuilder()
-		.setTitle(`Overall Salesperson Statistics as of ${today}:`)
-		.setDescription(overallDescList)
-		.setColor('5390D9');
-
-	let purchaseAgreementsEmbed = new EmbedBuilder()
-		.setTitle('Amount of Purchase Agreements Created:')
-		.setDescription(countPurchaseAgreements)
-		.setColor('4EA8DE');
-
-	let btnRows = addBtnRows();
-
-	client.embedMsg = await client.channels.cache.get(process.env.EMBED_CHANNEL_ID).send({ embeds: [overallStatsEmbed, purchaseAgreementsEmbed], components: btnRows });
-
-	await dbCmds.setMsgId("embedMsg", client.embedMsg.id);
 };
 
 function addBtnRows() {
