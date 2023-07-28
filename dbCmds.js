c8a2decfb4fe2956ea2834935d5012e6f327c3a4
Matthew Anthony <misterbaboo@gmail.com>
2023-07-27 22:13:55 -0400

    Clean up whitespace

--- dbCmds.js
+++ dbCmds.js
@@ -23,7 +23,6 @@ module.exports.resetSummValue = async (summaryName) => {
 	await summary.findOneAndUpdate({ summaryName: summaryName }, { value: 0 }, { upsert: true });
 };
 
-
 // for finding and adding to the salesperson's statistics
 module.exports.initPersStats = async (discordId, discordNickname) => {
 	await salesperson.findOneAndUpdate({ discordId: discordId }, { discordId: discordId, charName: discordNickname, purchaseAgreements: 0 }, { upsert: true });
@@ -33,7 +32,6 @@ module.exports.readPersStats = async (discordId) => {
 	let result = await salesperson.findOne({ discordId: discordId }, { discordId: 1, charName: 1, purchaseAgreements: 1, _id: 0 });
 	return result;
 };
-
 
 module.exports.addOnePersStat = async (discordId, statName) => {
 	await salesperson.findOneAndUpdate({ discordId: discordId }, { $inc: { [statName]: 1 } });
@@ -51,8 +49,6 @@ module.exports.setCharName = async (discordId, charName) => {
 	await salesperson.findOneAndUpdate({ discordId: discordId }, { charName: charName }, { upsert: true });
 };
 
-
-
 // for setting message ID of current Discord embed message
 module.exports.setMsgId = async (summaryName, newValue) => {
 	await summary.findOneAndUpdate({ summaryName: summaryName }, { msgId: newValue }, { upsert: true });
@@ -67,8 +63,6 @@ module.exports.readMsgId = async (summaryName) => {
 		return `Value not found for ${summaryName}.`;
 	}
 };
-
-
 
 module.exports.currStats = async () => {
 	let result = await salesperson.find({ charName: { $ne: null } }, { discordId: 1, charName: 1, purchaseAgreements: 1, _id: 0 });
